74fc29ec7c778811d8034fa9c94b82950ecef863
Sergei Smolianinov <lsdvitamin7@gmail.com>
2020-07-15 23:27:23 +0300

    Реализована новая версия Task-manager-1.0.1

--- src/main/java/ru/smolianinov/tm/App.java
+++ src/main/java/ru/smolianinov/tm/App.java
@@ -1,5 +1,7 @@
 package ru.smolianinov.tm;
 
+import java.util.Scanner;
+
 import static ru.smolianinov.tm.constant.TerminalConst.*;
 
 /*
@@ -11,46 +13,65 @@ public class App {
     public static void main(final String[] args) {
         run(args);
         displayWelcome();
+        final Scanner scanner = new Scanner(System.in);
+        String command = "";
+        while (!EXIT.equals(command)) {
+            command  = scanner.nextLine();
+            run(command);
+        }
     }
 
     private  static  void run(final String[] args) {
         if (args == null) return;
         if (args.length < 1) return;
         final String param = args[0];
+        final int result = run(param);
+        System.exit(result);
+    }
+
+    private  static  int run(final String param) {
+        if (param == null) return -1;
         switch (param) {
-            case VERSION: displayVersion();
-            case ABOUT: displayAbout();
-            case HELP: displayHelp();
-            default: displayError();
+            case VERSION: return  displayVersion();
+            case ABOUT: return displayAbout();
+            case HELP: return displayHelp();
+            case EXIT: return displayExit();
+            default: return displayError();
         }
 
     }
 
-    private static  void displayError() {
+    private static  int displayExit() {
+        System.out.println("Terminate program...");
+        return 0;
+    }
+
+    private static  int displayError() {
         System.out.println("Error! Unknown program argument...");
-        System.exit(-1);
+        return -1;
     }
 
     private static  void displayWelcome() {
         System.out.println("** WELCOME TO TASK MANAGER **");
     }
 
-    private static  void displayHelp() {
+    private static  int displayHelp() {
         System.out.println("version - Display application version.");
         System.out.println("about - Display developer info.");
         System.out.println("help - Display list of commands.");
-        System.exit( 0);
+        System.out.println("exit - Terminate console application.");
+        return 0;
     }
 
-    private static  void displayVersion() {
-        System.out.println("1.0.0");
-        System.exit( 0);
+    private static  int displayVersion() {
+        System.out.println("1.0.1");
+        return 0;
     }
 
-    private static  void displayAbout() {
+    private static  int displayAbout() {
         System.out.println("Sergei Smolianinov");
         System.out.println("sergei@gmail.com");
-        System.exit( 0);
+        return 0;
     }
 
 }
