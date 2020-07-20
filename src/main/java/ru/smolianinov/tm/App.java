c0598ad0e2f89bdda1de0c0a0257ce61e6a7504c
Sergey Smolianinov <lsdvitamin7@gmail.com>
2020-07-20 23:38:44 +0300

    Удалена папка doc, сделано форматирование кода, повышена версия maven.compiler до 11.

--- src/main/java/ru/smolianinov/tm/App.java
+++ src/main/java/ru/smolianinov/tm/App.java
@@ -16,12 +16,12 @@ public class App {
         final Scanner scanner = new Scanner(System.in);
         String command = "";
         while (!EXIT.equals(command)) {
-            command  = scanner.nextLine();
+            command = scanner.nextLine();
             run(command);
         }
     }
 
-    private  static  void run(final String[] args) {
+    private static void run(final String[] args) {
         if (args == null) return;
         if (args.length < 1) return;
         final String param = args[0];
@@ -29,33 +29,38 @@ public class App {
         System.exit(result);
     }
 
-    private  static  int run(final String param) {
+    private static int run(final String param) {
         if (param == null) return -1;
         switch (param) {
-            case VERSION: return  displayVersion();
-            case ABOUT: return displayAbout();
-            case HELP: return displayHelp();
-            case EXIT: return displayExit();
-            default: return displayError();
+            case VERSION:
+                return displayVersion();
+            case ABOUT:
+                return displayAbout();
+            case HELP:
+                return displayHelp();
+            case EXIT:
+                return displayExit();
+            default:
+                return displayError();
         }
 
     }
 
-    private static  int displayExit() {
+    private static int displayExit() {
         System.out.println("Terminate program...");
         return 0;
     }
 
-    private static  int displayError() {
+    private static int displayError() {
         System.out.println("Error! Unknown program argument...");
         return -1;
     }
 
-    private static  void displayWelcome() {
+    private static void displayWelcome() {
         System.out.println("** WELCOME TO TASK MANAGER **");
     }
 
-    private static  int displayHelp() {
+    private static int displayHelp() {
         System.out.println("version - Display application version.");
         System.out.println("about - Display developer info.");
         System.out.println("help - Display list of commands.");
@@ -63,12 +68,12 @@ public class App {
         return 0;
     }
 
-    private static  int displayVersion() {
+    private static int displayVersion() {
         System.out.println("1.0.1");
         return 0;
     }
 
-    private static  int displayAbout() {
+    private static int displayAbout() {
         System.out.println("Sergei Smolianinov");
         System.out.println("sergei@gmail.com");
         return 0;
